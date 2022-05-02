51595bdf0e72a06ad6ca080f2969969d71a03c20
Gaspar Giménez <gaspar.gimenez8@gmail.com>
2022-05-02 14:07:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -59,51 +59,74 @@
 			</div>
 		</div>
 	</nav>
-	
-	<div class="container-fluid container-inferior d-flex flex-column justify-content-center align-items-center">
-		<div class="carrusel">
-			<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-			<div class="carousel-indicators">
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-			</div>
-			<div class="carousel-inner">
-				<div class="carousel-item active ">
-				<img src="imagen0.jpg" class="d-block  overflow-hidden" alt="...">
-				<div class="carousel-caption d-none d-md-block ">
-					<h5>First slide label</h5>
-					<p>Some representative placeholder content for the first slide.</p>
-				</div>
-				</div>
-				<div class="carousel-item">
-				<img src="imagen2.jpg" class="d-block overflow-hidden" alt="...">
-				<div class="carousel-caption d-none d-md-block">
-					<h5>Second slide label</h5>
-					<p>Some representative placeholder content for the second slide.</p>
-				</div>
-				</div>
-				<div class="carousel-item">
-				<img src="imagen3.jpg" class="d-block overflow-hidden" alt="...">
-				<div class="carousel-caption d-none d-md-block">
-					<h5>Third slide label</h5>
-					<p>Some representative placeholder content for the third slide.</p>
-				</div>
-				</div>
-			</div>
-			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Previous</span>
-			</button>
-			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Next</span>
-			</button>
-			</div>
-		</div>
+			<!-- Carousel wrapper -->
+		<div id="carouselBasicExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
+		<!-- Indicators -->
+		<div class="carousel-indicators">
+			<button
+			type="button"
+			data-mdb-target="#carouselBasicExample"
+			data-mdb-slide-to="0"
+			class="active"
+			aria-current="true"
+			aria-label="Slide 1"
+			></button>
+			<button
+			type="button"
+			data-mdb-target="#carouselBasicExample"
+			data-mdb-slide-to="1"
+			aria-label="Slide 2"
+			></button>
+			<button
+			type="button"
+			data-mdb-target="#carouselBasicExample"
+			data-mdb-slide-to="2"
+			aria-label="Slide 3"
+			></button>
 		</div>
 
-	</div>
+		<!-- Inner -->
+		<div class="carousel-inner">
+			<!-- Single item -->
+			<div class="carousel-item active">
+			<img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" class="d-block w-100" alt="Sunset Over the City"/>
+			<div class="carousel-caption d-none d-md-block">
+				<h5>First slide label</h5>
+				<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+			</div>
+			</div>
+
+			<!-- Single item -->
+			<div class="carousel-item">
+			<img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" class="d-block w-100" alt="Canyon at Nigh"/>
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Second slide label</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			</div>
+			</div>
+
+			<!-- Single item -->
+			<div class="carousel-item">
+			<img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp" class="d-block w-100" alt="Cliff Above a Stormy Sea"/>
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Third slide label</h5>
+				<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+			</div>
+			</div>
+		</div>
+		<!-- Inner -->
+
+		<!-- Controls -->
+		<button class="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+		</div>
+		<!-- Carousel wrapper -->
 
 
 
